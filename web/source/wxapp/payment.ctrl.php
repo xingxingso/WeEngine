24664ae443eaeff478a236b162fdfb2044de4ea2
MYSELF\Administrator <1126973789@qq.com>
2017-06-15 17:03:52 +0800

    no message

---
+++ web/source/wxapp/payment.ctrl.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * [WeEngine System] Copyright (c) 2014 WE7.CC
+ * WeEngine is NOT a free software, it under the license terms, visited http://www.we7.cc/ for more details.
+ */
+defined('IN_IA') or exit('Access Denied');
+
+load()->model('account');
+load()->model('wxapp');
+
+$dos = array('get_setting', 'display');
+$do = in_array($do, $dos) ? $do : 'display';
+uni_user_permission_check('wxapp_payment', true, 'wxapp');
+$_W['page']['title'] = '支付参数';
+
+$pay_setting = wxapp_payment_param();
+
+if ($do == 'get_setting') {
+	iajax(0, $pay_setting, '');
+}
+
+if ($do = 'display') {
+	
+}
+template('wxapp/payment');
